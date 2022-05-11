8b68139e0d5766e218b30a1327c1e09d64a1502b
Lucas Andrade <la0359813@gmail.com>
2022-05-11 17:02:02 -0300

    fixed regex and application.yml

--- src/main/java/br/com/idp/lucas/project/dto/QuoteCreateRequest.java
+++ src/main/java/br/com/idp/lucas/project/dto/QuoteCreateRequest.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Map;
 
 @Data
@@ -15,5 +16,5 @@ public class QuoteCreateRequest {
     private String stockId;
 
     @NotEmpty
-    private Map<@Pattern(regexp = "\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])") String, @Pattern(regexp = "^[0-9]+(\\.[0-9]{2})?$") String> quotes;
+    private Map<@Pattern(regexp = "\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])") String, @Size(max = 255) @Pattern(regexp = "^[1-9]+[0-9]+(\\.[0-9]{2})+$") String> quotes;
 }
